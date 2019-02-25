d7c7de2dfae187394a3b825a26b9e2689c11e768
Geert Timmermans <geert.timmermans08@gmail.com>
2019-02-25 16:55:22 +0100

    .

--- js/Exercise-4/4.5.js
+++ js/Exercise-4/4.5.js
@@ -4,6 +4,9 @@ function calcDistance(x1, y1, x2, y2){
 }
 
 console.log(calcDistance (1,1,2,2));
+console.log(calcDistance (1,1,3,1));
+console.log(calcDistance (4,1,1,1));
+console.log(calcDistance (-2,2,2,-2));
 
 /* Create a function named calcDistance which takes the coordinates of two different points A and B in a 2D space. 
 That function must return the distance between those two points.
