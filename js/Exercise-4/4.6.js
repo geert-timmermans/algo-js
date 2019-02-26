7c38cb229878afa581a8048db742e10f08859f40
Geert Timmermans <geert.timmermans08@gmail.com>
2019-02-26 09:27:15 +0100

    .

--- js/Exercise-4/4.6.js
+++ js/Exercise-4/4.6.js
@@ -1,6 +1,15 @@
+function factorialize(num) {
+    if (num < 0) 
+          return -1;
+    else if (num == 0) 
+        return 1;
+    else {
+        return (num * factorialize(num - 1));
+    }
+  }
+  factorialize(5);
 
-
-
+console.log(factorialize(5));
 
 /* Create a function factorial(a) that returns the factorial of a number.
 
